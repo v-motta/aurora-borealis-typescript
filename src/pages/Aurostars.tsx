5416b775c828a59cdd890b6d40e2b0e52059fff5
viniciusc <vinicius.costa@trescon.com.br>
2023-01-24 20:31:14 -0300

    size aurostars

--- src/pages/Aurostars.tsx
+++ src/pages/Aurostars.tsx
@@ -10,9 +10,9 @@ const Aurostars = () => {
     <>
       <Navbar />
 
-      <div className="gray-bg text-white d-flex justify-content-center align-items-center p-5">
-        <div className="w-50">
-          <h1>O que é o Aurostars?</h1>
+      <div className="aurostars gray-bg text-white d-flex justify-content-center align-items-center" style={{ padding: "10rem 8rem" }}>
+        <div className="text-center me-5" style={{ width: "35%" }}>
+          <h1 className="fw-bold mb-5">O que é o Aurostars?</h1>
           <p>
             O <span className="fst-italic">Aurostars</span> é o projeto da <span className="fst-italic">Aurora Borealis</span> que consiste na produção de nossos Youtubers Virtuais, ou melhor,
             VTubers.
@@ -23,10 +23,12 @@ const Aurostars = () => {
 
       <PinkDivider />
 
-      <div className="gray-bg text-center text-white p-5">
-        <h1 className="p-5">Parece que as nossas estrelas ainda não estão prontas para serem reveladas.</h1>
-        <h1 className="p-5">Volte mais tarde.</h1>
-        <img src={stars} alt="" className="p-5" />
+      <div className="aurostars gray-bg d-flex flex-column align-items-center text-center text-white py-5">
+        <div style={{ width: "40%" }}>
+          <h1 className="p-5 fs-2">Parece que as nossas estrelas ainda não estão prontas para serem reveladas.</h1>
+          <h1 className="p-5 fs-2">Volte mais tarde.</h1>
+        </div>
+        <img src={stars} alt="" className="p-5 w-75" />
       </div>
 
       <PinkDivider />
