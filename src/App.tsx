283ff0a76e511952dc7c5e7aa262535447d423f7
viniciusc <vinicius.costa@trescon.com.br>
2023-01-24 14:05:54 -0300

    bootstrap icons and style file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./pages/style.css";
 import Router from "./config/router";
 
 function App() {
