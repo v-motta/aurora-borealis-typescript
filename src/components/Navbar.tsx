3d3d5b0119ab3ccf9380c2865588b73d59f86130
viniciusc <vinicius.costa@trescon.com.br>
2023-02-12 20:03:59 -0300

    mudando logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../assets/logo2.png";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
