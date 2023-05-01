b5dd49fb45ea7cece6bde6235ddf2f583d4bc4df
Vinicius Motta <viniciusmotta0806@gmail.com>
2023-05-01 12:15:59 -0300

    automation to update website

--- exec-nginx-files.sh
+++ exec-nginx-files.sh
@@ -1,7 +1,13 @@
+echo "Instalando pacotes"
 sudo npm i
 
+echo "Criando ambiente de produção"
 sudo npm run build
 
+echo "Copiando arquivos do /build/ para /var/www/html/"
 sudo cp -r /home/react-website/aurora-borealis-typescript/build/* /var/www/html/
 
+echo "Restartando o NGINX"
 sudo systemctl restart nginx
+
+echo "Site atualizado com sucesso"
